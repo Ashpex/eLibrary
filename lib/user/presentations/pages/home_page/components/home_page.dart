05eb3645ae0b9f9fd81c7870bfc0a53270e9b325
ashpex <ashpex@pm.me>
2022-06-02 11:02:24 +0700

    add user home page

---
+++ lib/user/presentations/pages/home_page/components/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:elibrary/models/newbook_model.dart';
+import 'package:elibrary/models/popularbook_model.dart';
+
+class UserHomePage extends StatefulWidget{
+  @override
+  _UserHomePageState extends  createState() => _HomeScreenState();
+
+
+}
+
+class _UserHomePageState extends  {
+}
