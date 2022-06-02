0d7694762d0995cbb7c57dbeb1d6109be390077a
ashpex <ashpex@pm.me>
2022-06-02 13:39:32 +0700

    fix main screen

--- lib/user/presentations/pages/profile_page/components/body.dart
+++ lib/user/presentations/pages/profile_page/components/body.dart
@@ -1,7 +1,25 @@
-import 'dart:ui';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:elibrary/models/User.dart';
 import 'package:elibrary/user/sources/color_constants.dart';
 import 'package:elibrary/user/presentations/pages/profile_page/components/header.dart';
 import 'package:elibrary/user/presentations/pages/profile_page/components/body.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  final User user;
+  ProfileScreen({required this.user});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+      ),
+      body: Center(
+        child: Text(
+          'Profile',
+          style: TextStyle(fontSize: 60),
+        ),
+      ),
+    );
+  }
+}
